03ee8201b4d2e26ca34deea10c015edb6deb8931
hautdog <agitell91@gmail.com>
2022-03-13 16:25:43 -0700

    made Database have 30 questions

--- src/model/SQLDatabase.java
+++ src/model/SQLDatabase.java
@@ -21,19 +21,46 @@ public class SQLDatabase {
 		//SQLDatabase DB = new SQLDatabase();
     	SQLiteDataSource DS = establishConnection("jdbc:sqlite:questions.db");
     	createEmptyTable(DS);
-    	addMultipleChoice(DS, "What year was the very first model of the iphone released?",
-    			"2007", "2005", "2007", "2008");
-    	addMultipleChoice(DS, "What is the shortcut to copy on most computers?", "ctrl c",
-    			"alt c", "shift c", "ctrl c");
-    	addMultipleChoice(DS, "What does HTTP stand for?", "hypertext transfer protocol",
-    			"hypertext transfer protocol", "hypertext transfer procedure", 
-    			"hyper transmitter protocol");
-    	addMultipleChoice(DS, "Who is often called the father of the computer?", "Charles Babbage",
-    			"Alan Touring", "Charles Babbage", "James Gosling");
-    	addMultipleChoice(DS, "Who discovered Penacillin?", "Alexander Flemming",
-    			"Dr. Frankenstein", "Albert Einstein", "Alexander Flemming");
-    	addTrueFalse(DS, "Java is a type of OS.", "false");
-    	addShortAnswer(DS, "What is the symbol for potassium?", "K");
+    	addMultipleChoice(DS, "Mario originated as a character in which video game?",
+    			"donkey kong", "donkey kong", "super mario", "mario party");
+    	addMultipleChoice(DS, "Which video game franchise uses V-bucks as currency?", "fortnite",
+    			"PUBG", "H1Z1", "fortnite");
+    	addMultipleChoice(DS, "What product did Nintendo originally sell?", "playing cards",
+    			"game consoles", "arcade games", "playing cards");
+    	addMultipleChoice(DS, "How many overworlds are in Cupworld?", "four",
+    			"two", "four", "seven");
+    	addMultipleChoice(DS, "To celebrate its 30th birthday in 2010, Google placed a playable version of what arcade game?", 
+    			"Pac-Man", "Asteroids", "Pac-Man", "Doodle Jump");
+    	addMultipleChoice(DS, "Which French video game company is publishing the Far Cry series?", "ubisoft",
+    			"ubisoft", "fromSoftware", "electronic arts");
+    	addMultipleChoice(DS, "In which game do players compete in the future version of soccer with cars?", "rocket league",
+    			"fifa", "mario kart", "rocket league");
+    	addMultipleChoice(DS, "What was Mario's first job?", "carpenter", "plumber", "carpenter", "electrician");
+    	addMultipleChoice(DS, "What year was the first Call of Duty game released?", "2003", "2000",
+    			"2007", "2003");
+    	addMultipleChoice(DS, "What's the best selling video game of all time?", "minecraft", "super smash bros",
+    			"minecraft", "halo 2");
+    	addTrueFalse(DS, "The final course in all of the mario kart games is rainbow road.", "true");
+    	addTrueFalse(DS, "The first nintendo console to use optical disks was the Wii.", "false");
+    	addTrueFalse(DS, "Blizzard Entertainment is most well known for the World of War Craft franchise.", "true");
+    	addTrueFalse(DS, "The US air force used the playstation 3 to create a cluster supercomputer.", "true");
+    	addTrueFalse(DS, "Game Boy is the best-selling handheld gaming system to date.", "false");
+    	addTrueFalse(DS, "The first virtual reality headset was created in 1995.", "true");
+    	addTrueFalse(DS, "Nintendo released the first flight simulator game.", "false");
+    	addTrueFalse(DS, "The creator of the Game Boy was a janitor at Nintendo.", "true");
+    	addTrueFalse(DS, "Mario's dinosaur sidekick is named bowser.", "false");
+    	addTrueFalse(DS, "Motion sickness plagues up to 40% of virtual reality users", "true");
+    	addShortAnswer(DS, "What was the first commercially successful video game?", "pong");
+    	addShortAnswer(DS, "Which Nintendo system first had a 007 video game?", "nintendo 64");
+    	addShortAnswer(DS, "What is the name of Crash Bandicoot's sister?", "naughty dog");
+    	addShortAnswer(DS, "What computer video game allowed you to play real life simulation in the 1990s?", "the sims");
+    	addShortAnswer(DS, "What Mortal Kombat character was based on a ninja?", "sub zero");
+    	addShortAnswer(DS, "Duke Nukem was a video game created by which game developer?", "apogee");
+    	addShortAnswer(DS, "What was the most popular arcade video game in the 1980s?", "pac man");
+    	addShortAnswer(DS, "GTA stands for what?", "grand theft auto");
+    	addShortAnswer(DS, "In the game ‘Fortnight Battle Royale’, how many players can play simultaneously in competition?",
+    			"100");
+    	addShortAnswer(DS, "The famous video game heroin Lara Croft belonged to which game?", "tomb raider");
     	ArrayList<Question> questions = createQuestionList(DS);
     	setMyQuestionList(questions);
     	//for (int i = 0; i < myQuestionList.size(); i++) {
